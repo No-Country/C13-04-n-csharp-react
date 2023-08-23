4524c7ce1a017e40e6a93278e86a14bf224e7944
lautaro0298 <lautaro.0298@gmail.com>
2023-08-23 03:38:58 -0300

    Proyecto creado y modelo y controler de usuario

---
+++ Backend/Cripto-divisas/Controllers/ConversorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cripto_divisas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConversorController : ControllerBase
+
+    {
+
+    }
+}
---
+++ Backend/Cripto-divisas/Migrations/20230823061614_initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cripto_divisas.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    usuarioId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    nombreUsuario = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    contraseña = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Key = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Vector = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.usuarioId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
