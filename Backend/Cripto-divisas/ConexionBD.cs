27657f7370e2b1b9d0851285add7bf76e458a364
Lautaro <Lautaro@DESKTOP-66NLO8G>
2023-08-22 21:28:39 -0300

    creo proyecto

---
+++ Backend/Cripto-divisas/ConexionBD.cs
@@ -0,0 +1,25 @@
+﻿using Cripto_divisas.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Reflection.Emit;
+using System.Reflection.PortableExecutable;
+
+namespace Cripto_divisas
+{
+    public class ConexionDB : DbContext
+    {
+        public ConexionDB()
+        {
+
+        }
+     
+        
+        public ConexionDB(DbContextOptions<ConexionDB> options): base(options)
+        {
+        }
+        public DbSet<Usuario> Usuario { get; set; }
+       
+    }
+}
+
---
+++ Backend/Cripto-divisas/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace Cripto_divisas.Models
+{
+    public class Usuario
+    {
+        public string usuarioId { get; set; }
+        public string nombreUsuario { get; set; }
+        public string email { get; set; }
+        public byte[] contraseña { get; set; }
+        public byte[] Key { get; set; }
+        public byte[] Vector { get; set; }
+
+    }
+}
+
