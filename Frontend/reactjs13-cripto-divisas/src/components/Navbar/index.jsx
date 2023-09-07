483fd13629181e624b7f658cb4f08bd8af50cc40
Neider Parra <neiderpb24@gmail.com>
2023-09-07 12:24:00 -0500

    actualizacion 07/09

--- Frontend/reactjs13-cripto-divisas/src/components/Navbar/index.jsx
+++ Frontend/reactjs13-cripto-divisas/src/components/Navbar/index.jsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import { useEffect } from "react";
-import { FaUser, FaSearch } from "react-icons/fa";
 
 function Navbar() {
   useEffect(() => {
@@ -36,13 +35,14 @@ function Navbar() {
         </button>
 
         <div className="collapse navbar-collapse " id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0"
+            style={{ paddingLeft: '10rem', fontSize: '20px' }}>
             <li className="nav-item mx-5">
               <Link
                 className="nav-link active"
                 aria-current="page"
                 href="/"
-                style={{ color: "#FF7C03", fontSize: "18px" }}
+                style={{ color: "#FF7C03" }}
               >
                 Convertidor
               </Link>
@@ -51,7 +51,7 @@ function Navbar() {
               <Link
                 className="nav-link"
                 href="/news"
-                style={{ color: "#FF7C03", fontSize: "18px" }}
+                style={{ color: "#FF7C03" }}
               >
                 Noticias
               </Link>
@@ -63,16 +63,17 @@ function Navbar() {
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
-                style={{ color: "#FF7C03", fontSize: "18px" }}
+                style={{ color: "#FF7C03" }}
               >
                 Como usar
               </Link>
-              <ul className="dropdown-menu" style={{ background: "#CAC7C7" }}>
+              <ul className="dropdown-menu"
+                style={{ background: "#CAC7C7", fontSize: '18px' }}>
                 <li>
                   <Link
                     className="dropdown-item"
                     href="/howToUse/cryptoCurrency"
-                    style={{ color: "#FF7C03", fontSize: "18px" }}
+                    style={{ color: "#FF7C03" }}
                   >
                     Criptomonedas
                   </Link>
@@ -82,7 +83,7 @@ function Navbar() {
                   <Link
                     className="dropdown-item"
                     href="/howToUse/currency"
-                    style={{ color: "#FF7C03", fontSize: "18px" }}
+                    style={{ color: "#FF7C03" }}
                   >
                     Divisas
                   </Link>
@@ -94,7 +95,7 @@ function Navbar() {
                   <Link
                     className="dropdown-item"
                     href="/howToUse/trading"
-                    style={{ color: "#FF7C03", fontSize: "18px" }}
+                    style={{ color: "#FF7C03" }}
                   >
                     Trading
                   </Link>
@@ -107,48 +108,13 @@ function Navbar() {
                 className="nav-link active"
                 aria-current="page"
                 href="/convert"
-                style={{ color: "#FF7C03", fontSize: "18px" }}
+                style={{ color: "#FF7C03" }}
               >
                 Enviar
               </Link>
             </li>
           </ul>
-          <form className="d-flex ms-auto" role="search">
-            <input
-              className="form-control me-1 border border-light"
-              type="search"
-              placeholder="Buscar"
-              aria-label="Search"
-              style={{ fontSize: "16px", backgroundColor: "#CAC7C7" }}
-            />
-            <button
-              className="btn btn-outline-secondary custom-button "
-              type="submit"
-            >
-              <FaSearch style={{ verticalAlign: "middle" }} />
-            </button>
-          </form>
 
-          <style jsx>{`
-            .custom-button {
-              height: 2.7rem;
-              font-size: 18px;
-              background: #a7a7a7;
-              color: #ff7c03;
-              display: flex;
-              align-items: center;
-            }
-          `}</style>
-
-          <div className="d-flex ms-auto align-items-center">
-            <button
-              className="btn btn-outline-whatever"
-              type="button"
-              style={{ fontSize: "1.5rem" }}
-            >
-              <FaUser style={{ fontSize: "24px", color: "#FF7C03" }} />
-            </button>
-          </div>
         </div>
       </div>
     </nav>
