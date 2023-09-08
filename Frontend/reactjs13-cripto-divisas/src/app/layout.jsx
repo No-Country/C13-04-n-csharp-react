cff5c8caefea559631a6f64f8366226a357249fd
JORGE ALEXANDER ARDILA RESTREPO <jorge.ardila1641@correo.policia.gov.co>
2023-09-08 11:34:29 -0500

    se edita el layout

--- Frontend/reactjs13-cripto-divisas/src/app/layout.jsx
+++ Frontend/reactjs13-cripto-divisas/src/app/layout.jsx
@@ -2,8 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "@/components/Navbar";
 
 export const metadata = {
-  title: "Divisas y criptomonedas App",
-  description: "Sistema de estadísticas para divisas y criptomonedas",
+  title: "Cripto Divisas App",
+  description: "Sistema de conversión entre divisas y criptomonedas",
 };
 
 export default function RootLayout({ children }) {
